2ad8850c148b8014313c70f631c65a0529bd0bc4
Alexander Markov <markov_av_77@mail.ru>
2024-08-27 06:16:10 +0400

    First edition of the second task

---
+++ 1_2_reverse_order.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cout << "\033[2J\033[1;1H";
+    cout << "This program takes some numbers from user and get them back in reverse order\n";
+    cout << "How many elements will be contain your array?\n";
+    int n;
+    cin >> n;
+    vector<float> array(n);
+    cout << "Input elements\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+    cout << "Putting the array in reverse order\n";
+for (int i = 0; i < n; i++)
+    {
+        cout << array[n - i];
+    }
+}
