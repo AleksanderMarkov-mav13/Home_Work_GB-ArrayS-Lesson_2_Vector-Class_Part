25b7c5940bb5f22a76f0e21f91223e53ebddd38d
Alexander Markov <markov_av_77@mail.ru>
2024-08-27 07:00:11 +0400

    Convert to the work version and add control input

--- 1_3_find_submax.cpp
+++ 1_3_find_submax.cpp
@@ -7,14 +7,18 @@ int main()
     cout << "This program takes several numbers from the user and finds the second maximum element\n";
     cout << "How many elements will be contain your array?\n";
     int n, max = 0, submax = 0;
-    // cin >> n;
-    n = 7;
+    cin >> n;
+    if (n < 2) {
+        cout << "Size of your array can not be less, then two elements!\n";
+        cout << "Repeat your enter!\n";
+        cin >> n;
+    }
     vector<int> array(n);
     cout << "Input elements\n";
-    array = {5, 4, 6, 7, 8, 12, 16}; //
+    // array = {16, 14, 6, 7, 8, 16, 10}; // string for comfortable testing
     for (int i = 0; i < n; i++)
     {
-        // cin >> array[i];
+        cin >> array[i];
         if (array[i] > max)
         {
             submax = max;
