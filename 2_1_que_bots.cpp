6abe55c1c9d55f930cc0318e24781d43467faa78
Alexander Markov <markov_av_77@mail.ru>
2024-08-30 19:02:17 +0400

    Second desicion -- with funktion

--- 2_1_que_bots.cpp
+++ 2_1_que_bots.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+vector <int> add_vec (vector <int> vec, int n){
+    vector <int> newvec(vec.size()+1);
+    for (int i = 0; i < vec.size(); i++)
+    {
+        newvec[i] = vec[i];
+    }
+    newvec[vec.size()] = n;
+    return newvec;
+}
 int main()
 {
     cout << "\033[2J\033[1;1H";
     cout << "This program is designed to get, memorize and print a list of bots\n";
     vector <int> vec = {12};
     int num = 0;
-    int count = 1;
+    // int count = 1;
     while (num != -1) {
         cout << "Input your number, bot  ";
         cin >> num;
-        count ++;
-        vector<int> newvec(count);
-
-        for (int i = 0; i < count - 1; i++) {
-            newvec[i] = vec[i];
-        }
-        newvec[count - 1] = num;
-        vec = newvec;
+        vec = add_vec(vec, num);
     }
 
     cout << "\n We have got such array of bots numbers: \n";
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < vec.size (); i++)
     {
         cout << vec[i] << " ";
     }
