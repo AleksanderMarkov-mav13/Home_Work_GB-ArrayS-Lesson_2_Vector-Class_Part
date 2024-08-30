2f7b301681b15f93f8c4d88019f99f19201d5682
Alexander Markov <markov_av_77@mail.ru>
2024-08-30 19:07:17 +0400

    File 2_2: start

---
+++ 2_2_clinica.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cout << "\033[2J\033[1;1H";
+    cout << "This program is designed to get and analize a list of patients\n";
+    vector <int> ages = {0};
+    
+}
