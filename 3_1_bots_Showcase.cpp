af16bf423665a628188514c08de14942a97adf83
Alexander Markov <markov_av_77@mail.ru>
2024-09-01 18:22:15 +0400

    Return

--- 3_1_bots_Showcase.cpp
+++ 3_1_bots_Showcase.cpp
@@ -4,15 +4,16 @@ using namespace std;
 vector<int> erase_bot(vector<int> vec, vector<int> bot_sell, int val_less)
 {
     vector<int> newvec(vec.size() - val_less);
-    int count_sell = 0;
+    int count_less = 0;
     for (int i = 0; i < vec.size(); i++)
     {
-        for (int j = 0; j < bot_sell.size(); j++){
-            if (i == bot_sell[j] - 1) count_sell++; 
-            else {
-                newvec[i - count_sell] = vec[i];
-                break;
-            }
+        if (i == bot_sell[count_less] - 1)
+        {
+            count_less++;
+        }
+        else
+        {
+            newvec[i - count_less] = vec[i];
         }
     }
     return newvec;
@@ -26,18 +27,20 @@ int main()
     cin >> val_bot;
     vector<int> bot_list(val_bot);
     cout << "Input serial number of bot № \n";
-     for (int i = 0; i < val_bot; ++i)
+    for (int i = 0; i < val_bot; ++i)
     {
         cout << i + 1 << "  - ";
         cin >> bot_list[i];
-        // cout << "\n";
+        cout << "\n";
     }
     cout << "How many bots are we going to cell?\n";
-    do {
+    do
+    {
         cin >> val_less;
-        if (val_less <= 0 || val_less > val_bot) cout << "Wrong input!\n";
+        if (val_less <= 0 || val_less > val_bot)
+            cout << "Wrong input!\n";
     } while (val_less <= 0 || val_less > val_bot);
-    vector <int> bot_sell(val_less);
+    vector<int> bot_sell(val_less);
     cout << "Input the index of bot, that we are celling\n";
     for (int i = 0; i < val_less; ++i)
     {
@@ -45,8 +48,8 @@ int main()
     }
     bot_list = erase_bot(bot_list, bot_sell, val_less);
     cout << "Now we have on our showcase next list of boots:\n";
-    for (int i = 0; i < bot_list.size (); i++)
+    for (int i = 0; i < bot_list.size(); i++)
     {
-        cout << "№ " << i + 1 << "  S/N " << bot_list [i] << "\n";
+        cout << "№ " << i + 1 << "  S/N " << bot_list[i] << "\n";
     }
 }
