fb3a2bc2d9af81a10debd14dc5f3a7e421baae0c
Alexander Markov <markov_av_77@mail.ru>
2024-09-08 08:36:17 +0400

    Add base part

--- 4_1_add_que_bots.cpp
+++ 4_1_add_que_bots.cpp
@@ -5,4 +5,14 @@ int main()
 {
     cout << "\033[2J\033[1;1H";
     cout << "This program is simulator shop of bots\n";
+    vector <int> que;
+    int number = 0;
+    while (number != -1) {
+        cin >> number;
+        que.push_back(number);
+    } 
+    for (int i = 0; i < que.size(); i++) {
+        cout << que[i] << " ";
+    }
+
 }
