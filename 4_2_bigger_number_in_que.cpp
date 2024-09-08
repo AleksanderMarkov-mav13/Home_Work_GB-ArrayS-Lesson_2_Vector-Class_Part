12c8b1a3133facb736c02f1599cda9cc9809b78e
Alexander Markov <markov_av_77@mail.ru>
2024-09-08 10:58:18 +0400

    Final task 4_2А

--- 4_2_bigger_number_in_que.cpp
+++ 4_2_bigger_number_in_que.cpp
@@ -4,16 +4,19 @@ using namespace std;
 int main()
 {
     cout << "\033[2J\033[1;1H";
-    cout << "This program is simulator que of bots\n";
+    cout << "This program is simulator que of bots, where bot with bigger number erase bots with smoller numbers\n";
     vector<int> que;
     int number = 0;
     cout << "Input your numbers to get in the que, bots\n";
     while (true)
     {
         cin >> number;
-        if (number == -1)
-            break;
+        while (que.size() != 0 && que[que.size() - 1] < number)
+        {
+            que.pop_back();
+        }
         que.push_back(number);
+        if (number == -1) break;
     }
     for (int i = 0; i < que.size(); i++)
     {
