966194348a7f089d6db82ac359d5392e87add8f1
Alexander Markov <markov_av_77@mail.ru>
2024-09-08 10:46:11 +0400

    Add base from other file

---
+++ 4_2_bigger_number_in_que.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cout << "\033[2J\033[1;1H";
+    cout << "This program is simulator que of bots\n";
+    vector<int> que;
+    int number = 0;
+    cout << "Input your numbers to get in the que, bots\n";
+    while (true)
+    {
+        cin >> number;
+        if (number == -1)
+            break;
+        que.push_back(number);
+    }
+    for (int i = 0; i < que.size(); i++)
+    {
+        cout << que[i] << " ";
+    }
+}
