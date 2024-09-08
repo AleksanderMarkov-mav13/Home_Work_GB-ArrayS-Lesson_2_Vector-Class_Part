b7b4cee00ec54ed0a6ced6c8a7dd6192c0d2dd2f
Alexander Markov <markov_av_77@mail.ru>
2024-09-08 12:15:42 +0400

    Base of task 4_3

---
+++ 4_3_room_boots.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    cout << "\033[2J\033[1;1H";
+    // cout << "This program is simulator que of bots, where bot with bigger number erase bots with smoller numbers\n";
+    vector<int> que;
+    int number = 0;
+    cout << "Input your numbers to get in the que, bots\n";
+    while (true)
+    {
+        cin >> number;
+        if (number == -1) break;
+        que.push_back(number);
+        if (que.capacity () - que.size() <= 2)
+            cout << "Warning! Free space in the room is running out!";
+        }
+    for (int i = 0; i < que.size(); i++)
+    {
+        cout << que[i] << " ";
+    }
+}
