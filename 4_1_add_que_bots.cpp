931dbd8f05ff0d278c65827a27ffe4f73b0a917e
Alexander Markov <markov_av_77@mail.ru>
2024-09-08 08:44:34 +0400

    Final task 4_1

--- 4_1_add_que_bots.cpp
+++ 4_1_add_que_bots.cpp
@@ -4,12 +4,14 @@ using namespace std;
 int main()
 {
     cout << "\033[2J\033[1;1H";
-    cout << "This program is simulator shop of bots\n";
+    cout << "This program is simulator que of bots\n";
     vector <int> que;
     int number = 0;
-    while (number != -1) {
+    cout << "Input your numbers to get in the que, bots\n";
+    while (true) {
         cin >> number;
-        que.push_back(number);
+        if (number == -1) break;
+            que.push_back(number);
     } 
     for (int i = 0; i < que.size(); i++) {
         cout << que[i] << " ";
